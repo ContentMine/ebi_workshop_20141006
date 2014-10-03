3a7422f254e65fc94e7a5bd5c50ba67e387beace
Richard Smith-Unna <rds45@cam.ac.uk>
2014-10-03 11:20:23 +0100

    add colour to update script

--- setup/latest_updates.sh
+++ setup/latest_updates.sh
@@ -1,32 +1,52 @@
-echo "Now updating all ContentMine software."
+red='\e[0;31m'
+blue='\e[0;32m'
+green='\e[0;34m'
+lightgreen='\e[1;34m'
+NC='\e[0m' # No Color
 
-echo password | sudo -S echo "(sudo authentication successful)"
+echo -e "${red}Hello Stackoverflow${NC}"
+
+echo -e "${blue}Now updating all ContentMine software.${NC}"
+
+echo password | sudo -S echo -e "${lightgreen}(sudo authentication successful)${NC}"
 
 ## Workshop files
-cd ~/ebi_workshop_20141006
-git pull
+echo -e "${blue}Updating workshop files...${NC}"
+cd ~/ebi_workshop_20141006 1>/dev/null 2>/dev/null
+git pull 1>/dev/null 2>/dev/null
+echo -e "${lightgreen}done.${NC}"
 
 ## XHTML2STM
-wget https://bitbucket.org/petermr/xhtml2stm/downloads/xhtml2stm_0.4~SNAPSHOT_all.deb
-sudo dpkg -i xhtml2stm_0.4~SNAPSHOT_all.deb
-rm xhtml2stm_0.4~SNAPSHOT_all.deb
+echo -e "${blue}Updating XHTML2STM...${NC}"
+wget https://bitbucket.org/petermr/xhtml2stm/downloads/xhtml2stm_0.4~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
+sudo dpkg -i xhtml2stm_0.4~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
+rm xhtml2stm_0.4~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
+echo -e "${lightgreen}done.${NC}"
 
 ## ChemExtractor
-wget https://bitbucket.org/mjw99/chemextractorpublic/downloads/chemextractor_1.0~SNAPSHOT_all.deb
-sudo dpkg -i chemextractor_1.0~SNAPSHOT_all.deb
-rm chemextractor_1.0~SNAPSHOT_all.deb
+echo -e "${blue}Updating ChemExtractor...${NC}"
+wget https://bitbucket.org/mjw99/chemextractorpublic/downloads/chemextractor_1.0~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
+sudo dpkg -i chemextractor_1.0~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
+rm chemextractor_1.0~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
+echo -e "${lightgreen}done.${NC}"
 
 ## Quickscrape
-npm update --global quickscrape
+echo -e "${blue}Updating quickscrape...${NC}"
+npm update --global quickscrape 1>/dev/null 2>/dev/null
+echo -e "${lightgreen}done.${NC}"
 
 ## Journal-scrapers
-cd ~/journal-scrapers
-git pull
+echo -e "${blue}Updating journal-scrapers...${NC}"
+cd ~/journal-scrapers 1>/dev/null 2>/dev/null
+git pull 1>/dev/null 2>/dev/null
+echo -e "${lightgreen}done.${NC}"
 
+echo -e "${blue}Installing any missing software...${NC}"
 ## Tesseract-OCR
-sudo apt-get install -y tesseract-ocr
+sudo apt-get install -y tesseract-ocr 1>/dev/null 2>/dev/null
 
 ## Other useful installs
-sudo apt-get install -y mercurial maven
+sudo apt-get install -y mercurial maven 1>/dev/null 2>/dev/null
+echo -e "${lightgreen}done.${NC}"
 
-echo "All ContentMine software updates completed."
+echo -e "${green}All ContentMine software updates completed.${NC}"
