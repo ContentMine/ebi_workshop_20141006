4c22d01097ebe56936f86ec521c921df7d5b87d1
Richard Smith-Unna <rds45@cam.ac.uk>
2014-10-03 11:30:50 +0100

    message and completion on same line

--- setup/latest_updates.sh
+++ setup/latest_updates.sh
@@ -1,5 +1,5 @@
 red='\e[0;31m'
-blue='\e[0;34m'
+blue='\e[1;34m'
 green='\e[0;32m'
 lightgreen='\e[1;32m'
 NC='\e[0m' # No Color
@@ -7,40 +7,39 @@ NC='\e[0m' # No Color
 echo -e "${blue}Now updating all ContentMine software.${NC}"
 
 echo password | sudo -S echo "" 1>/dev/null 2>/dev/null
-echo -e "${lightgreen}(sudo authentication successful)${NC}"
 
 ## Workshop files
-echo -e "${blue}Updating workshop files...${NC}"
+echo -ne "${blue}Updating workshop files...${NC}  "
 cd ~/ebi_workshop_20141006 1>/dev/null 2>/dev/null
 git pull 1>/dev/null 2>/dev/null
 echo -e "${lightgreen}done.${NC}"
 
 ## XHTML2STM
-echo -e "${blue}Updating XHTML2STM...${NC}"
+echo -ne "${blue}Updating XHTML2STM...${NC}  "
 wget https://bitbucket.org/petermr/xhtml2stm/downloads/xhtml2stm_0.4~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
 sudo dpkg -i xhtml2stm_0.4~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
 rm xhtml2stm_0.4~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
 echo -e "${lightgreen}done.${NC}"
 
 ## ChemExtractor
-echo -e "${blue}Updating ChemExtractor...${NC}"
+echo -ne "${blue}Updating ChemExtractor...${NC}  "
 wget https://bitbucket.org/mjw99/chemextractorpublic/downloads/chemextractor_1.0~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
 sudo dpkg -i chemextractor_1.0~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
 rm chemextractor_1.0~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
 echo -e "${lightgreen}done.${NC}"
 
 ## Quickscrape
-echo -e "${blue}Updating quickscrape...${NC}"
+echo -ne "${blue}Updating quickscrape...${NC}  "
 npm update --global quickscrape 1>/dev/null 2>/dev/null
 echo -e "${lightgreen}done.${NC}"
 
 ## Journal-scrapers
-echo -e "${blue}Updating journal-scrapers...${NC}"
+echo -ne "${blue}Updating journal-scrapers...${NC}  "
 cd ~/journal-scrapers 1>/dev/null 2>/dev/null
 git pull 1>/dev/null 2>/dev/null
 echo -e "${lightgreen}done.${NC}"
 
-echo -e "${blue}Installing any missing software...${NC}"
+echo -ne "${blue}Installing any missing software...${NC}  "
 ## Tesseract-OCR
 sudo apt-get install -y tesseract-ocr 1>/dev/null 2>/dev/null
 
