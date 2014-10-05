6cd89801c93739a6301cd969890b9601a5c9e634
Richard Smith-Unna <rds45@cam.ac.uk>
2014-10-05 18:01:09 +0200

    include ami2-poc

--- setup/latest_updates.sh
+++ setup/latest_updates.sh
@@ -28,6 +28,13 @@ sudo dpkg -i chemextractor_1.0~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
 rm chemextractor_1.0~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
 echo -e "${lightgreen}done.${NC}"
 
+## AMI2-POC
+echo -ne "${blue}Updating AMI2-POC...${NC}  "
+wget https://bitbucket.org/petermr/xhtml2stm/downloads/ami2-poc_0.0.2~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
+sudo dpkg -i ami2-poc_0.0.2~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
+rm ami2-poc_0.0.2~SNAPSHOT_all.deb 1>/dev/null 2>/dev/null
+echo -e "${lightgreen}done.${NC}"
+
 ## Quickscrape
 echo -ne "${blue}Updating quickscrape...${NC}  "
 npm update --global quickscrape 1>/dev/null 2>/dev/null
